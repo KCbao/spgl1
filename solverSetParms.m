c2036f75cb2ae3a46daec56d2c1158eda547f97a
Casie <abao@math.ubc.ca>
2019-08-25 17:13:16 -0700

    contains only 4 qp solvers

--- solverSetParms.m
+++ solverSetParms.m
@@ -18,8 +18,7 @@ function argout = solverSetParms(mode, p, A)
              argout{3} = options;
          
          case 2 
-             % pdco setup
-             
+             % pdco setup    
              beq = sparse(2*n, 1); 
              bl = [-Inf(p+2+1,1);sparse(2*n,1)]; 
              bu = Inf(p+2+1+2*n,1);
@@ -39,42 +38,34 @@ function argout = solverSetParms(mode, p, A)
              argout{6} = options;
              
              
-         case 4
+         case 3
              % Gurobi
-             
-         case 5 
-             % qpopt setup (not recommand)
-             H = eye(p+2+1,p+2+1);
-             H(p+2+1,p+2+1) = 0;
-             x0 = zeros(p+2+1,1);
-             bu = zeros(2*n+p+2+1,1);
-             bu(p+2+1) = +inf;
-             bl = -inf(2*n+p+2+1,1);
-             argout = cell(6,1);
-             argout{1} = H;
-             argout{2} = x0;
-             argout{3} = bl;
-             argout{4} = bu;
-             
-         case 6
-             % dpopt setup
+             argout{1} = [];
+         case 4
+             % dnopt setup
              H = eye(p+2+1,p+2+1);
              H(p+2+1,p+2+1) = 0;
     
-             x0 = zeros(p+2+1,1); %initialization
+             x0 = zeros(p+2+1,1); % initialization
              x0(p+2+1) = 1;
              xl = -inf(p+2+1,1);
              xu = inf(p+2+1,1);
              bu = zeros(2*n,1);
              bl = -inf(2*n,1);
-            
-             argout = cell(6,1);
+             
+             options.printfile = '';
+             options.screen = 'off';
+             options.optimality_tolerance = 1e-06;
+             options.iterations_limit = 100;  
+             
              argout{1} = H;
              argout{2} = x0;
              argout{3} = xl;
              argout{4} = xu;
              argout{5} = bl;
              argout{6} = bu;
+             argout{7} = options;
+             
          otherwise
             disp('other value')
     end
