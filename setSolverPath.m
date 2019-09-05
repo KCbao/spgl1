bf27db8e579b608bf5109233ef95e4e81da6d1b8
Casie <abao@math.ubc.ca>
2019-09-05 14:32:14 -0700

    mute my local path dir

--- setSolverPath.m
+++ setSolverPath.m
@@ -4,13 +4,9 @@ function []= setSolverPath(solverNum, solver_path_dir)
    
     switch solverNum
         case 2
-            % pdco_dir = /path/to/pdco/solver
-%             pdco_dir = '/Users/casiebao/Dropbox/Research/spgl1/pdco-master';
             addpath([solver_path_dir,'/code'                 ],'-end');
             
         case 4
-            % dnopt_dir = /path/to/dnopt/solver
-%             dnopt_dir = '/Users/casiebao/Dropbox/Research/spgl1/dnopt';
             addpath([solver_path_dir,'/matlab'              ],'-end');
             addpath([solver_path_dir,'/matlab/util'         ],'-end');
             addpath([solver_path_dir,'/matlab/precompiled'  ],'-end');
